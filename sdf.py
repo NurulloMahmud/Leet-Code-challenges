d58e8bc245a16b6878a3b773ae0bb7bfdba2953c
nurulloh <nurullomahmud@gmail.com>
2023-09-20 01:06:15 +0500

    initial commit

---
+++ sdf.py
@@ -0,0 +1,27 @@
+class Solution(object):
+    def lengthOfLongestSubstring(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        letters = [i for i in s]
+        characters=[]
+        characters_lenths=[]
+        for c in letters:
+            if c in characters:
+                indx_of_av_char = characters.index(c)
+                characters_lenths.append(len(characters))
+                characters = characters[indx_of_av_char+1:]
+                print(characters)
+            characters.append(c)
+        if len(characters)>0:
+
+            return max(characters_lenths)
+        print(characters)
+        return 0
+    
+a=Solution()
+
+# print(a.lengthOfLongestSubstring(' '))
+
+print(len([' ']))
